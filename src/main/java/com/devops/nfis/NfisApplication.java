8dc0556797cb2336b30a9bfc115780f2758a5a13
Bhagya Rathnayake <rathnayake.bhagya94@gmail.com>
2021-10-19 00:03:49 +0530

    initial commit

---
+++ src/main/java/com/devops/nfis/NfisApplication.java
@@ -0,0 +1,13 @@
+package com.devops.nfis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/nfis/NfisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.nfis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
